30085937514a11ce05f63bce49f1c4c63348152a
Michael Johnson <kojo.johnson44@gmail.com>
2023-12-05 00:01:35 +0000

    services list update

--- app/amenities/page.jsx
+++ app/amenities/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { EB_Garamond, Nunito_Sans } from "next/font/google";
+import Services from "../components/Services";
 
 const eb_garamond = EB_Garamond({
   subsets: ["latin"],
@@ -23,8 +24,8 @@ const nunito_sans = Nunito_Sans({
 
 export default function Amenities() {
   return (
-    <section className="mt-[9.5rem]">
-      <div className="hero-bg h-[250px] md:h-[470px] text-white flex flex-col gap-4 justify-center items-center text-center px-6">
+    <section className="mt-[8.7rem]">
+      <div className="hero-bg h-[250px] md:h-[470px] text-white flex flex-col ease duration-250 gap-4 justify-center items-center text-center px-6">
         <p className={`${eb_garamond.className} text-[1.58rem] md:text-5xl`}>
           You Can Have It All Here
         </p>
@@ -37,7 +38,9 @@ export default function Amenities() {
       <div className="mt-7 md:mt-[80px] mx-8 flex flex-col gap-7 md:flex-row">
         <hr className="w-[96%] md:w-[250px] mt-5 gold_color"></hr>
         <div className="md:mr-[14rem] md:ml-5">
-          <h3 className={`${nunito_sans_bold.className} text-2xl`}>
+          <h3
+            className={`${nunito_sans_bold.className} text-2xl text-[#383838]`}
+          >
             Facilities & Services
           </h3>
           <p className="mt-3">
@@ -47,6 +50,7 @@ export default function Amenities() {
           </p>
         </div>
       </div>
+      <Services />
     </section>
   );
 }
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LiaSwimmingPoolSolid } from "react-icons/lia";
 import { FaCar } from "react-icons/fa";
 import {
@@ -9,5 +11,74 @@ import { GiLift } from "react-icons/gi";
 import { ImConnection } from "react-icons/im";
 
 export default function Services() {
-  return <div></div>;
+  return (
+    <section className="bg-[#F3EEEA] flex flex-wrap justify-center px-8 py-14 gap-9 md:gap-12 mt-14 ">
+      <div className="w-[27rem]">
+        <ImConnection size={27} className="text-[#bc9142]" />
+        <div>
+          <h3 className="text-[1.125rem] font-medium mb-2">High Speed Wifi</h3>
+          <p className="text-justify">
+            Stay connected seamlessly on our lightning fast internet
+            connectivity that caters to the demands of your modern lifestyle;
+            ideal for residents who study and/or work remotely.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[27rem]">
+        <FaCar size={27} className="text-[#bc9142]" />
+        <h3 className="text-[1.125rem] font-medium mb-2">Parking Space</h3>
+        <p className="text-justify">
+          Our spacious and well-maintained parking facilities ensure a
+          stress-free experience, providing you with the peace of mind that
+          comes with knowing your vehicle is safe and accessible right at home.{" "}
+        </p>
+      </div>
+      <div className="w-[27rem]">
+        <LiaSwimmingPoolSolid size={27} className="text-[#bc9142]" />
+        <h3 className="text-[1.125rem] font-medium mb-2">Swimming Pool</h3>
+        <p>
+          Dive into a world of relaxation and socialization in our inviting pool
+          area.{" "}
+        </p>
+      </div>
+      <div className="w-[27rem]">
+        <MdSecurity size={27} className="text-[#bc9142] mb-1" />
+        <h3 className="text-[1.125rem] font-medium mb-2">24/7 Security</h3>
+        <p className="text-justify">
+          Your safety is our top priority at Inno Towers, where state-of-the-art
+          CCTV surveillance and our vigilant security team blend to provide
+          peace of mind.{" "}
+        </p>
+      </div>
+      <div className="w-[27rem]">
+        <MdOutlineElectricalServices size={27} className="text-[#bc9142]" />
+        <h3 className="text-[1.125rem] font-medium mb-2">Backup Utilities</h3>
+        <p className="text-justify">
+          We understand that continuity is key, which is why our complex is
+          equipped with robust backup systems for power and water supply.{" "}
+        </p>
+      </div>
+      <div className="w-[27rem]">
+        <GiLift size={27} className="text-[#bc9142]" />
+        <h3 className="text-[1.125rem] font-medium mb-2">Lifts</h3>
+        <p className="text-justify">
+          Whether you are arriving home with groceries or just tired from work,
+          our meticulously maintained elevators promise effortless glides
+          between all floors.{" "}
+        </p>
+      </div>
+    </section>
+  );
+}
+
+{
+  /* <div className="w-[27rem]">
+        <MdLocalLaundryService size={27} className="text-[#bc9142]" />
+        <h3 className="text-[1.125rem] font-medium">Laundry Services</h3>
+        <p>
+          Our professional laundry services ensure your clothes are cared for
+          with the utmost attention, leaving you with more time to enjoy the
+          things that matter.{" "}
+        </p>
+      </div> */
 }
