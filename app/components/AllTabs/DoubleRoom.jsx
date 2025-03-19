7884a8400de6bee8fa2d6f7ae0576bd323f26ecf
spacegod1 <kojo.johnson44@gmail.com>
2025-03-19 16:45:36 +0000

    checking images

--- app/components/AllTabs/DoubleRoom.jsx
+++ app/components/AllTabs/DoubleRoom.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import twobedroom from "@/public/double.jpg";
-import typeA from "@/public/Type-A.jpg";
+import typeA from "@/public/Type-A.png";
 import typeB from "@/public/Type-B.png";
 import typeC from "@/public/Type-C.png";
 import { Play } from "next/font/google";
